c472d6e0443bccde4e1639eabb3cbdc4853f60b6
J. Charles N. M. <jcnm@sylorion.com>
2025-07-09 08:50:55 +0200

    Maj de la logique de gestion des messages dans un fichier unique

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,9 +4,11 @@
 export { useWebSocket, useWebSocketMessages } from './use-websocket';
 export { useTypingIndicator } from './use-typing-indicator';
 
+// Hook de messaging unifié (NOUVEAU - point unique de messagerie)
+export { useMessaging } from './use-messaging';
+
 // Traduction unifiée
-export { useTranslation, useMessageTranslation, useSimpleTranslation } from './use-translation';
-export { useTranslationCache } from './use-translation-cache';
+export { useTranslation } from './use-translation';
 
 // Interface utilisateur et notifications
 export { useNotifications } from './use-notifications';
