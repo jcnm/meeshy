9d2e1729ed2609db6bdd11c017a58061e255099e
J. Charles N. M. <jcnm@sylorion.com>
2025-08-15 11:44:06 +0200

    cleanup: Supprimer les derniers fichiers de types compilés obsolètes
    - Supprimer les fichiers .js et .map restants dans frontend/shared/types/
    - Supprimer les fichiers .js et .map restants dans gateway/shared/types/
    - Finaliser le nettoyage des types compilés

--- gateway/shared/types/socketio-events.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Types unifiés pour les événements Socket.IO Meeshy
- * Remplace les anciens types WebSocket pour correspondre à la nouvelle architecture Socket.IO
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=socketio-events.js.map
